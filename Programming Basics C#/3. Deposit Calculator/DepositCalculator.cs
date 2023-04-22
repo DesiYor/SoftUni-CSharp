cb27a576d49c9577fe9aa674128640e705d20bd8
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-22 15:14:39 +0100

    update

---
+++ Programming Basics C#/3. Deposit Calculator/DepositCalculator.cs
@@ -0,0 +1,35 @@
+﻿/* 
+ *  Напишете програма, която изчислява каква сума ще получите в края на депозитния период при определен лихвен процент.
+ *  Използвайте следната формула: 
+    сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
+    Вход
+    От конзолата се четат 3 реда:
+    ⦁	Депозирана сума – реално число в интервала [100.00 … 10000.00]
+    ⦁	Срок на депозита (в месеци) – цяло число в интервала [1…12]
+    ⦁	Годишен лихвен процент – реално число в интервала [0.00 …100.00]
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Deposit_Calculator
+{
+
+    internal class DepositCalculator
+    {
+        static void Main(string[] args)
+        {
+            double sum = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine()); 
+            double interest = double.Parse(Console.ReadLine()) / 100; 
+
+            double result = sum + months * ((sum * interest) /12);
+
+            Console.WriteLine(result);
+        }
+    }
+}
