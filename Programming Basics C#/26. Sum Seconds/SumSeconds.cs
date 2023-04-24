d5187e57a17c0c687eda02e1bb4ea82feb60ee73
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-24 01:53:58 +0100

    Update SumSeconds.cs

--- Programming Basics C#/26. Sum Seconds/SumSeconds.cs
+++ Programming Basics C#/26. Sum Seconds/SumSeconds.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ ⦁	Сумиране на секунди 
+Трима спортни състезатели финишират за някакъв брой секунди (между 1 и 50). Да се напише програма, която чете времената
+на състезателите в секунди, въведени от потребителя и пресмята сумарното им време във формат "минути:секунди". 
+Секундите да се изведат с водеща нула (2  "02", 7  "07", 35  "35"). 
+
+ */
+
+using System;
+
 
 namespace _26.Sum_Seconds
 {
-    internal class Program
+    internal class SumSeconds
     {
         static void Main(string[] args)
         {
