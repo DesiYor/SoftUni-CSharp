b5fabad9dc96281b80e1cf41cf6bead5db23a203
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-25 15:29:21 +0100

    Update TradeCommissions.cs

--- Programming Basics C#/45. Trade Commissions/TradeCommissions.cs
+++ Programming Basics C#/45. Trade Commissions/TradeCommissions.cs
@@ -20,6 +20,35 @@ namespace _45.Trade_Commissions
     {
         static void Main(string[] args)
         {
+            string town = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+
+            switch (town)
+            {
+                case "Sofia":
+                    if ( sales >= 0 && sales <= 500 ) { Console.WriteLine($"{ sales * 0.05:f2}"); }
+                    else if ( sales > 500 && sales <= 1000) { Console.WriteLine($"{sales * 0.07:f2}"); }
+                    else if ( sales > 1000 && sales <= 10000) { Console.WriteLine($"{sales * 0.08:f2}"); }
+                    else if (sales > 10000) { Console.WriteLine($"{sales * 0.12:f2}"); }
+                    else { Console.WriteLine("error"); break; }
+                    break;
+                case "Varna":
+                    if (sales >= 0 && sales <= 500) { Console.WriteLine($"{sales * 0.045:f2}"); }
+                    else if (sales > 500 && sales <= 1000) { Console.WriteLine($"{sales * 0.075:f2}"); }
+                    else if (sales > 1000 && sales <= 10000) { Console.WriteLine($"{sales * 0.1:f2}"); }
+                    else if (sales > 10000) { Console.WriteLine($"{sales * 0.13:f2}"); }
+                    else { Console.WriteLine("error"); break; }
+                    break;
+                case "Plovdiv":
+                    if (sales >= 0 && sales <= 500) { Console.WriteLine($"{sales * 0.055:f2}"); }
+                    else if (sales > 500 && sales <= 1000) { Console.WriteLine($"{sales * 0.08:f2}"); }
+                    else if (sales > 1000 && sales <= 10000) { Console.WriteLine($"{sales * 0.12:f2}"); }
+                    else if (sales > 10000) { Console.WriteLine($"{sales * 0.145:f2}"); }
+                    else { Console.WriteLine("error"); break; }
+                    break;
+                default: Console.WriteLine("error"); break;
+            }
+            Console.ReadLine();
         }
     }
 }
