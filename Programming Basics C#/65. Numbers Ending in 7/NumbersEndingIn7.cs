06879695fd522d99550265458f32f1974d1c7f8c
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-27 00:47:16 +0100

    update

---
+++ Programming Basics C#/65. Numbers Ending in 7/NumbersEndingIn7.cs
@@ -0,0 +1,22 @@
+﻿/*
+ ⦁	Числа до 1000, завършващи на 7
+Напишете програма, която отпечатва числата в диапазона от 1 до 1000, които завършват на 7.
+
+ */
+
+using System;
+
+
+namespace _65.Numbers_Ending_in_7
+{
+    internal class NumbersEndingIn7
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 1000; i++)
+            {
+                if (i % 10 == 7) { Console.WriteLine(i); }
+            }
+        }
+    }
+}
