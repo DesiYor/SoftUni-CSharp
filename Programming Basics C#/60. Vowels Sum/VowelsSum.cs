b0f262806b30bf6bcd2236803490a4572f40ddfe
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-26 22:20:21 +0100

    Update VowelsSum.cs

--- Programming Basics C#/60. Vowels Sum/VowelsSum.cs
+++ Programming Basics C#/60. Vowels Sum/VowelsSum.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ ⦁	Сумиране на гласните букви
+Да се напише програма, която чете текст (стринг), въведен от потребителя, и изчислява и отпечатва сумата от стойностите на гласните букви
+според таблицата по-долу:
+буква	    a	e	i	o	u
+стойност	1	2	3	4	5
+
+ */
+
+using System;
+
 
 namespace _60.Vowels_Sum
 {
-    internal class Program
+    internal class VowelsSum
     {
         static void Main(string[] args)
         {
+            char[] text = Console.ReadLine().ToCharArray();
+            int total = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+                if (text[i] == 'a') { total++; }
+                if (text[i] == 'e') { total += 2; }
+                if (text[i] == 'i') { total += 3; }
+                if (text[i] == 'o') { total += 4; }
+                if (text[i] == 'u') { total += 5; }
+            }
+            Console.ReadLine();
         }
     }
 }
