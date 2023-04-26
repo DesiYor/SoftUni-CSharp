d8a82a8abe39bc054a53df91ff8fde678761a99a
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-26 23:51:29 +0100

    update

--- Programming Basics C#/63. Left and Right Sum/LeftAndRightSum.cs
+++ Programming Basics C#/63. Left and Right Sum/LeftAndRightSum.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ ⦁	Лява и дясна сума
+Да се напише програма, която чете 2*n-на брой цели числа, подадени от потребителя,
+и проверява дали сумата на първите n числа (лява сума) е равна на сумата на вторите n числа (дясна сума).
+При равенство печата " Yes, sum = " + сумата; иначе печата " No, diff = " + разликата.
+Разликата се изчислява като положително число (по абсолютна стойност). 
+ */
+
+using System;
+
 
 namespace _63.Left_and_Right_Sum
 {
-    internal class Program
+    internal class LeftAndRightSum
     {
         static void Main(string[] args)
         {
+            int loop = int.Parse(Console.ReadLine());
+            int numberLeft = 0;
+            int numberRight = 0;
+            for (int i = 0; i < loop; i++)
+            {
+                numberLeft += int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < loop; i++)
+            {
+                numberRight += int.Parse(Console.ReadLine());
+            }
+            if (numberLeft == numberRight) { Console.WriteLine($"Yes, sum = {numberRight}"); }
+            else { Console.WriteLine($"No, diff = {Math.Abs(numberLeft-numberRight)}"); }
         }
     }
 }
---
+++ Programming Basics C#/64. Odd Even Sum/OddEvenSum.cs
@@ -0,0 +1,34 @@
+﻿/*
+ ⦁	Четна / нечетна сума
+Да се напише програма, която чете n-на брой цели числа, подадени от потребителя 
+и проверява дали сумата от числата на четни позиции е равна на сумата на числата на нечетни позиции. 
+⦁	Ако сумите са равни да се отпечатат два реда: "Yes" и на нов ред "Sum = " + сумата; 
+⦁	Ако сумите не са равни да се отпечат два реда: "No" и на нов ред "Diff = " + разликата. 
+Разликата се изчислява по абсолютна стойност. 
+
+ */
+
+using System;
+
+
+namespace _64.Odd_Even_Sum
+{
+    internal class OddEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int loop = int.Parse(Console.ReadLine());
+            int numberEven = 0;
+            int numberOdd = 0;
+            for (int i = 0; i < loop; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0) { numberEven += number; }
+                else { numberOdd += number; }
+
+            }
+            if (numberEven == numberOdd) { Console.WriteLine($"Yes"); Console.WriteLine($"Sum = {numberOdd}"); }
+            else { Console.WriteLine($"No"); Console.WriteLine($"Diff = {Math.Abs(numberOdd - numberEven)}"); }
+        }
+    }
+}
