10724c7f5f87be52f5d64ff80880cf990f10d667
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-28 02:57:53 +0100

    Update Coins.cs

--- Programming Basics C#/85. Coins/Coins.cs
+++ Programming Basics C#/85. Coins/Coins.cs
@@ -15,6 +15,7 @@ namespace _85.Coins
         static void Main(string[] args)
         {
             double money = double.Parse(Console.ReadLine());
+            /*
             int[] coins = new int[8];
             coins[0] = Convert.ToInt32(Math.Floor(money)) / 2; // монетите по 2 
             coins[1] = Convert.ToInt32(Math.Floor(money)) - (2 * coins[0]); // монети по 1
@@ -29,10 +30,28 @@ namespace _85.Coins
             {
                 if (coins[i] > 0) { count += coins[i]; }
             }
-
             Console.WriteLine(count);
-            Console.ReadLine();
-
+            */
+            //
+            //
+            //
+            //По бърза версия
+            int allCoins = Convert.ToInt32(money * 100); // 
+            int[] c = new int[8]; // 8 вида монети
+            c[0] = allCoins / 200; // монети по 2
+            c[1] = (allCoins - (200 * c[0])) / 100; //монети по 1
+            c[2] = (allCoins - (200 * c[0]) - (100 * c[1])) / 50 ; // монети по 50
+            c[3] = (allCoins - (200 * c[0]) - (100 * c[1]) - (50 * c[2])) / 20; //монети по 20
+            c[4] = (allCoins - (200 * c[0]) - (100 * c[1]) - (50 * c[2]) - (20 * c[3])) / 10; //монети по 10
+            c[5] = (allCoins - (200 * c[0]) - (100 * c[1]) - (50 * c[2]) - (20 * c[3]) - (10 * c[4])) / 5; //монети по 5
+            c[6] = (allCoins - (200 * c[0]) - (100 * c[1]) - (50 * c[2]) - (20 * c[3]) - (10 * c[4]) - (5 * c[5])) / 2; // монети по 2
+            c[7] = (allCoins - 200 * c[0]) - (100 * c[1]) - (50 * c[2]) - (20 * c[3]) - (10 * c[4]) - (5 * c[5]) - (2 * c[6]); // монети по 1
+            int count = 0;
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] > 0) { count += c[i]; }
+            }
+            Console.WriteLine(count);
         }
     }
 }
