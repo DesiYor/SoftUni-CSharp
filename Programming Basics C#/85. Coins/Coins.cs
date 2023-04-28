ff2e5a3a3de0009899aeac4bd77f55b3e33a9e41
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-28 03:06:52 +0100

    Update Coins.cs

--- Programming Basics C#/85. Coins/Coins.cs
+++ Programming Basics C#/85. Coins/Coins.cs
@@ -15,27 +15,6 @@ namespace _85.Coins
         static void Main(string[] args)
         {
             double money = double.Parse(Console.ReadLine());
-            /*
-            int[] coins = new int[8];
-            coins[0] = Convert.ToInt32(Math.Floor(money)) / 2; // монетите по 2 
-            coins[1] = Convert.ToInt32(Math.Floor(money)) - (2 * coins[0]); // монети по 1
-            coins[2] = Convert.ToInt32((money - Math.Floor(money)) * 100) / 50; // монети по 50
-            coins[3] = (Convert.ToInt32((money - Math.Floor(money)) * 100) - (50 * coins[2])) / 20; // монети по 20
-            coins[4] = (Convert.ToInt32((money - Math.Floor(money)) * 100) - (50 * coins[2]) - (20 * coins[3])) / 10; // монети по 10
-            coins[5] = (Convert.ToInt32((money - Math.Floor(money)) * 100) - (50 * coins[2]) - (20 * coins[3]) - (10 * coins[4])) / 5; //монети по 5
-            coins[6] = (Convert.ToInt32((money - Math.Floor(money)) * 100) - (50 * coins[2]) - (20 * coins[3]) - (10 * coins[4]) - (5 * coins[5])) / 2; // монети по 2
-            coins[7] = Convert.ToInt32((money - Math.Floor(money)) * 100) - (50 * coins[2]) - (20 * coins[3]) - (10 * coins[4]) - (5 * coins[5]) - (2 * coins[6]); // монети по 1
-            int count = 0;
-            for (int i = 0; i < coins.Length; i++)
-            {
-                if (coins[i] > 0) { count += coins[i]; }
-            }
-            Console.WriteLine(count);
-            */
-            //
-            //
-            //
-            //По бърза версия
             int allCoins = Convert.ToInt32(money * 100); // 
             int[] c = new int[8]; // 8 вида монети
             c[0] = allCoins / 200; // монети по 2
