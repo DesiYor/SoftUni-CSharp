3dcd113bf9f58b772313740a5cf7c590bc415498
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-29 20:12:17 +0100

    update

--- Programming Basics C#/85. Coins/Coins.cs
+++ Programming Basics C#/85. Coins/Coins.cs
@@ -6,7 +6,7 @@
  */
 
 using System;
-
+using System.Diagnostics;
 
 namespace _85.Coins
 {
@@ -14,8 +14,11 @@ namespace _85.Coins
     {
         static void Main(string[] args)
         {
-            double money = double.Parse(Console.ReadLine());
-            int allCoins = Convert.ToInt32(money * 100); // 
+
+            int allCoins = Convert.ToInt32(double.Parse(Console.ReadLine()) * 100);
+            int backup = allCoins; // да държи пърмоначалния вход не променен
+
+            //Решение 1 - не добро спрямо заделено място в рам 
             int[] c = new int[8]; // 8 вида монети
             c[0] = allCoins / 200; // монети по 2
             c[1] = (allCoins - (200 * c[0])) / 100; //монети по 1
@@ -30,7 +33,44 @@ namespace _85.Coins
             {
                 if (c[i] > 0) { count += c[i]; }
             }
-            Console.WriteLine(count);
+
+            Console.WriteLine($"A1: {count} no loop");
+            
+            //Решение 2 -- най-добро решения спрямо призводителност за момента 
+
+            int counts = 0;
+            if (allCoins >= 200) { counts += allCoins / 200; allCoins = allCoins % 200; } // монети по 2лв 
+            if (allCoins >= 100) { counts += allCoins / 100; allCoins = allCoins % 100; }
+            if (allCoins >= 50) { counts += allCoins / 50; allCoins = allCoins % 50; }
+            if (allCoins >= 20) { counts += allCoins / 20; allCoins = allCoins % 20; }
+            if (allCoins >= 10) { counts += allCoins / 10; allCoins = allCoins % 10; }
+            if (allCoins >= 5) { counts += allCoins / 5; allCoins = allCoins % 5; }
+            if (allCoins >= 2) { counts += allCoins / 2; allCoins = allCoins % 2; }
+            if (allCoins >= 0) { counts += allCoins; }
+
+            Console.WriteLine($"A2: {counts} no loop");
+
+            // Решение 3 използвайки loop - не удачно спрямо производителснот 
+
+            allCoins = backup;
+
+            int counts2 = 0;
+            int loopCount = 0;
+            while (allCoins != 0) 
+            { 
+                if(allCoins - 200 >= 0) { allCoins -= 200; counts2++; }  
+                else if (allCoins - 100 >= 0) { allCoins -= 100; counts2++; }
+                else if (allCoins - 50 >= 0) { allCoins -= 50; counts2++; }
+                else if (allCoins - 20 >= 0) { allCoins -= 20; counts2++; }
+                else if (allCoins - 10 >= 0) { allCoins -= 10; counts2++; }
+                else if (allCoins - 5 >= 0) { allCoins -= 5; counts2++; }
+                else if (allCoins - 2 >= 0) { allCoins -= 2; counts2++; }
+                else if (allCoins - 1 >= 0) { allCoins -= 1; counts2++; }
+                loopCount++;
+            }
+
+            Console.WriteLine($"A3: {counts2} with {loopCount} loops");
+            Console.ReadLine();
         }
     }
 }
---
+++ Programming Basics C#/86. Cake/Cake.cs
@@ -0,0 +1,43 @@
+﻿/*
+ ⦁	Торта
+Поканени сте на 30-ти рожден ден, на който рожденикът черпи с огромна торта. Той обаче не знае колко парчета могат да си вземат гостите от нея.
+Вашата задача е да напишете програма, която изчислява броя на парчетата, които гостите са взели, преди тя да свърши.
+Ще получите размерите на тортата (широчина и дължина – цели числа в интервала [1...1000]) и след това на всеки ред,
+до получаване на командата "STOP" или докато не свърши тортата, броят на парчетата, които гостите вземат от нея. 
+Бележка: Едно парче торта е с размер 1х1 см.
+Да се отпечата на конзолата един от следните редове:
+⦁	"{брой парчета} pieces are left." - ако стигнете до STOP и не са свършили парчетата торта
+⦁	"No more cake left! You need {брой недостигащи парчета} pieces more."
+
+ 
+ */
+
+
+using System;
+
+
+namespace _86.Cake
+{
+    internal class Cake
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            bool loop = true;
+            int pieces = width * height;
+            while (loop)
+            {
+                string take = Console.ReadLine();
+                switch (take)
+                {
+                    case "STOP": loop = false; Console.WriteLine($"{pieces} pieces are left."); break;
+                    default:
+                        if (pieces - int.Parse(take) > 0) { pieces -= int.Parse(take); }
+                        else { Console.WriteLine($"No more cake left! You need {Math.Abs(pieces - int.Parse(take))} pieces more."); loop = false; }
+                        break;
+                }
+            }
+        }
+    }
+}
