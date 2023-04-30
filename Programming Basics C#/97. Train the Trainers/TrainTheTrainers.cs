b25d169abebd9ffd34f777036bbd6525af7a01dc
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-04-30 16:41:42 +0100

    update

--- Programming Basics C#/97. Train the Trainers/TrainTheTrainers.cs
+++ Programming Basics C#/97. Train the Trainers/TrainTheTrainers.cs
@@ -22,7 +22,22 @@ namespace _97.Train_the_Trainers
     {
         static void Main(string[] args)
         {
-
+            int ppl = int.Parse(Console.ReadLine());
+            double finalAss = 0;
+            int countAss = 0;
+            while(true)
+            {
+                
+                string pres = Console.ReadLine();
+                if (pres == "Finish") { Console.WriteLine($"Student's final assessment is {finalAss/countAss:f2}."); break; }
+                double grade = 0.00;
+                countAss++;
+                for (int i = 1; i <= ppl; i++) 
+                {
+                    grade += double.Parse(Console.ReadLine());
+                    if (i == ppl) { finalAss += grade / ppl; Console.WriteLine($"{pres} - {grade / ppl:f2}."); break; }
+                } 
+            }
         }
     }
 }
