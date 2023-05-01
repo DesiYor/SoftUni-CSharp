cf0d09dba782364f31948e20c52a3a7c1407eaaa
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-05-01 20:56:20 +0100

    Update Cinema.cs

--- Programming Basics C# Exams/04. Cinema/Cinema.cs
+++ Programming Basics C# Exams/04. Cinema/Cinema.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _04.Cinema
 {
-    internal class Program
+    internal class Cinema
     {
         static void Main(string[] args)
         {
+            int places = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int profit = 0;
+            bool stop = true;
+            while (command != "Movie time!")
+            {
+                int people = int.Parse(command);
+                places -= people;
+                if (places < 0) { Console.WriteLine("The cinema is full."); stop = false; break; }
+                if (people % 3 == 0) { profit += people * 5 - 5; }
+                else { profit += people * 5; }
+                command = Console.ReadLine();
+            }
+            if(stop) { Console.WriteLine($"There are {places} seats left in the cinema."); }
+            Console.WriteLine($"Cinema income - {profit} lv.");
+            Console.ReadLine();
         }
     }
 }
