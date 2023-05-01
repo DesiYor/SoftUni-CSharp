d24ee5edafe78aaa0b05523cf613f83c585bd5c5
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-05-01 04:17:48 +0100

    update

---
+++ Programming Basics C# Exams/03. Film Premiere/FilmPremiere.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System;
+
+
+namespace _03.Film_Premiere
+{
+    internal class FilmPremiere
+    {
+        static void Main(string[] args)
+        {
+            string film = Console.ReadLine();
+            string menu = Console.ReadLine();
+            int tickets = int.Parse(Console.ReadLine());
+            double bill = 0;
+            switch(film)
+            {
+                case "John Wick": 
+                    switch(menu)
+                    {
+                        case "Drink": bill = tickets * 12.00;  break;
+                        case "Popcorn": bill = tickets * 15.00; break;
+                        case "Menu": bill = tickets * 19.00; break;
+                        default: break;
+                    }
+                    break;
+                case "Star Wars":
+                    switch (menu)
+                    {
+                        case "Drink": bill = tickets * 18.00; if (tickets >= 4) { bill = bill * (1 - 0.30); } break;
+                        case "Popcorn": bill = tickets * 25.00; if (tickets >= 4) { bill = bill * (1 - 0.30); } break;
+                        case "Menu": bill = tickets * 30.00; if (tickets >= 4) { bill = bill * (1 - 0.30); } break;
+                        default: break;
+                    }
+                    break;
+                case "Jumanji":
+                    switch (menu)
+                    {
+                        case "Drink": bill = tickets * 9.00; if (tickets == 2) { bill = bill * (1 - 0.15); } break;
+                        case "Popcorn": bill = tickets * 11.00; if (tickets == 2) { bill = bill * (1 - 0.15); } break;
+                        case "Menu": bill = tickets * 14.00; if (tickets == 2) { bill = bill * (1 - 0.15); } break;
+                        default: break;
+                    }
+                    break;
+                default: break;
+            }
+            Console.WriteLine($"Your bill is {bill:f2} leva.");
+
+        }
+    }
+}
---
+++ Programming Basics C# Exams/03. Movie Destination/MovieDestination.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace _03.Movie_Destination
+{
+    internal class MovieDestination
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string destination = Console.ReadLine();
+            string season = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            double bill = 0;
+
+            switch (destination)
+            {
+                case "Dubai":
+                    switch(season)
+                    {
+                        case "Summer": bill = (40000 * days) * (1 - 0.30); break;
+                        case "Winter": bill = (45000 * days) * (1 - 0.30); break;
+                        default: break;
+                    }
+                    break;
+                case "Sofia":
+                    switch (season)
+                    {
+                        case "Summer": bill = (12500 * days) * (1 + 0.25); break;
+                        case "Winter": bill = (17000 * days) * (1 + 0.25); break;
+                        default: break;
+                    }
+                    break;
+                case "London":
+                    switch (season)
+                    {
+                        case "Summer": bill = 20250 * days; break;
+                        case "Winter": bill = 24000 * days; break;
+                        default: break;
+                    }
+                    break;
+                default: break;
+            }
+            if(budget >= bill)
+            {
+                Console.WriteLine($"The budget for the movie is enough! We have {budget-bill:f2} leva left!");
+            }
+            else { Console.WriteLine($"The director needs {bill - budget:f2} leva more!"); }
+            Console.ReadLine();
+        }
+    }
+}
