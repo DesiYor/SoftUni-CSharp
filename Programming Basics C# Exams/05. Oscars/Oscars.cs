c989fee9fd5b111c68b3f4b2c1c1127aa773b6d3
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-05-01 22:06:09 +0100

    update

---
+++ Programming Basics C# Exams/05. Oscars/Oscars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _05.Oscars
+{
+    internal class Oscars
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double startPoints = double.Parse(Console.ReadLine());
+            int focusGroup = int.Parse(Console.ReadLine());
+            for(int i = 0; i < focusGroup; i++)
+            {
+                string nameFgroup = Console.ReadLine();
+                double points = double.Parse(Console.ReadLine());
+                startPoints += (nameFgroup.Length * points / 2);
+                if (startPoints >= 1250.5) break;
+            }
+            if(startPoints >= 1250.5) { Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {startPoints:f1}!"); }
+            else { Console.WriteLine($"Sorry, {actorName} you need {1250.5 - startPoints:f1} more!"); }
+            Console.ReadLine();
+        }
+    }
+}
--- Programming Basics C# Exams/05. Series/Series.cs
+++ Programming Basics C# Exams/05. Series/Series.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _05.Series
 {
-    internal class Program
+    internal class Series
     {
         static void Main(string[] args)
         {
+            double budget = double.Parse(Console.ReadLine());
+            int films = int.Parse(Console.ReadLine());
+            for (int i = 0; i < films; i++)
+            {
+                string filmName = Console.ReadLine();
+                double filmPrice = double.Parse(Console.ReadLine());
+                switch (filmName)
+                {
+                    case "Thrones": budget -= filmPrice * (1 - 0.5);  break;
+                    case "Lucifer": budget -= filmPrice * (1 - 0.4); break;
+                    case "Protector": budget -= filmPrice * (1 - 0.3); break;
+                    case "TotalDrama": budget -= filmPrice * (1 - 0.2); break;
+                    case "Area": budget -= filmPrice * (1 - 0.1); break;
+                    default: budget -= filmPrice; break; 
+                }
+            }
+            if(budget >= 0) { Console.WriteLine($"You bought all the series and left with {budget:f2} lv."); }
+            else { Console.WriteLine($"You need {Math.Abs(budget):f2} lv. more to buy the series!"); }
         }
     }
 }
