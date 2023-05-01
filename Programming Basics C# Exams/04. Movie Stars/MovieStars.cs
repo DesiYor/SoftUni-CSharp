7d3e04fe1b5a9d7922b4928fb08fb06f5b32650d
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-05-01 21:31:30 +0100

    update

---
+++ Programming Basics C# Exams/04. Movie Stars/MovieStars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace _04.Movie_Stars
+{
+    internal class MovieStars
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            while(true)
+            {
+                string name = Console.ReadLine();
+                if(name == "ACTION") { Console.WriteLine($"We are left with {budget:f2} leva."); break; }
+                if (name.Length > 15)
+                {
+                    budget -= budget * 0.20;
+                    if(budget < 0) { Console.WriteLine($"We need {Math.Abs(budget):f2} leva for our actors."); break; }
+                }
+                else
+                {
+                    budget -= double.Parse(Console.ReadLine());
+                    if (budget < 0) { Console.WriteLine($"We need {Math.Abs(budget):f2} leva for our actors."); break; }
+                }
+            }
+        }
+    }
+}
