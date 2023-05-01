13a961210eb568fb6dc88418d6f7a4ed72ea7232
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-05-02 00:24:47 +0100

    update

--- Programming Basics C# Exams/05. Oscars/Oscars.cs
+++ Programming Basics C# Exams/05. Oscars/Oscars.cs
@@ -15,11 +15,10 @@ namespace _05.Oscars
                 string nameFgroup = Console.ReadLine();
                 double points = double.Parse(Console.ReadLine());
                 startPoints += (nameFgroup.Length * points / 2);
-                if (startPoints >= 1250.5) break;
+                if (startPoints >= 1250.5) { break; }
             }
             if(startPoints >= 1250.5) { Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {startPoints:f1}!"); }
             else { Console.WriteLine($"Sorry, {actorName} you need {1250.5 - startPoints:f1} more!"); }
-            Console.ReadLine();
         }
     }
 }
---
+++ Programming Basics C# Exams/06. Favorite Movie/FavoriteMovie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _06.Favorite_Movie
+{
+    internal class FavoriteMovie
+    {
+        static void Main(string[] args)
+        {
+            string film = "", hold = "";
+            int counter = 0, sum = 0, maxSum = 0;
+            while (true)
+            {
+                if(counter >= 7) { Console.WriteLine("The limit is reached."); break; }
+                else counter++;
+                film = Console.ReadLine();
+                if(film == "STOP") { break; }
+                for(int i = 0; i < film.Length; i++)
+                {
+                    if (film[i] >= 65 && film[i] <= 90) { sum += Convert.ToInt32(film[i]) - film.Length; }
+                    else if (film[i] >= 97 && film[i] <= 122) { sum += Convert.ToInt32(film[i]) - film.Length * 2;  }
+                    else { sum += Convert.ToInt32(film[i]); }
+                }
+                if (maxSum <= sum) { maxSum = sum; hold = film; }
+                sum = 0;
+            }
+            Console.WriteLine($"The best movie for you is {hold} with {maxSum} ASCII sum.");
+        }
+    }
+}
