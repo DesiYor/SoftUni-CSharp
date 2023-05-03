fc7a2f99547446984a626a677f2aefd93c9463cb
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-05-03 19:42:38 +0100

    update

---
+++ Programming Basics C# Exams/01. Easter Bakery/EasterBakery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.Easter_Bakery
+{
+    internal class EasterBakery
+    {
+        static void Main(string[] args)
+        {
+            double priceFlour = double.Parse(Console.ReadLine());
+            double flour = double.Parse(Console.ReadLine());
+            double sugar = double.Parse(Console.ReadLine());
+            double eggs = double.Parse(Console.ReadLine());
+            double m = double.Parse(Console.ReadLine());
+            double totalF = priceFlour * flour;
+            double totalS = (priceFlour * (1 - 0.25)) * sugar;
+            double totalE = (priceFlour * (1 + 0.10)) * eggs;
+            double totalM = ((priceFlour * (1 - 0.25)) * (1 - 0.80)) * m;
+
+            Console.WriteLine($"{totalE + totalF + totalM + totalS:f2}");
+        }
+    }
+}
---
+++ Programming Basics C# Exams/03. Painting Eggs/PaintingEggs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace _03.Painting_Eggs
+{
+    internal class PaintingEggs
+    {
+        static void Main(string[] args)
+        {
+            string size = Console.ReadLine();
+            string colour = Console.ReadLine();
+            int pcs = int.Parse(Console.ReadLine()), total = 0;
+            switch (colour)
+            {
+                case "Red":
+                    switch(size)
+                    {
+                        case "Large": total += pcs * 16; break;
+                        case "Medium": total += pcs * 13; break;
+                        case "Small": total += pcs * 9; break;
+                        default: break;
+                    }
+                    break;
+                case "Green":
+                    switch (size)
+                    {
+                        case "Large": total += pcs * 12; break;
+                        case "Medium": total += pcs * 9; break;
+                        case "Small": total += pcs * 8; break;
+                        default: break;
+                    }
+                    break;
+                case "Yellow":
+                    switch (size)
+                    {
+                        case "Large": total += pcs * 9; break;
+                        case "Medium": total += pcs * 7; break;
+                        case "Small": total += pcs * 5; break;
+                        default: break;
+                    }
+                    break;
+                default: break;
+            }
+            Console.WriteLine($"{(double)total * (1 - 0.35):f2} leva.");
+        }
+    }
+}
