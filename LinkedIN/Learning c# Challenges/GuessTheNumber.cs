5167d3b6b7a9aa1a491d9ff2163547d8ffdd80b1
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-05-06 19:13:55 +0100

    update

---
+++ LinkedIN/Learning c# Challenges/GuessTheNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+
+namespace Learning_c__Challenges
+{
+    internal class GuessTheNumber
+    {
+        static void Main(string[] args)
+        {
+            int number = new Random().Next(20), count = 0;
+            Console.WriteLine("Guess the number!");
+            Console.WriteLine("I am thinking of a number between 0 and 20!");
+            Console.WriteLine("Try to guess the number or enter -1 to give up!");
+            while (true) 
+            {
+                try
+                {
+                    int guess = int.Parse(Console.ReadLine());
+                    count++;
+                    if (guess == number)
+                    {
+                        Console.WriteLine($"Congrats! You guessed the number {number} on your {count} try!"); break;
+                    }
+                    else if (guess == -1) { Console.WriteLine($"You give up!"); break; }
+                    else { Console.WriteLine($"Wrong! Try Count: {count}"); }
+                }
+                catch 
+                {
+                    Console.WriteLine("You must enter number between 0 and 20! Program do not accept strings as input! Try again!");
+                    continue;
+                }
+            } 
+
+
+
+        }
+    }
+}
---
+++ LinkedIN/Palindrome/Palindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Palindrome
+{
+    internal class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets begin!");
+
+            (bool , int , string ) Palidrome(string strr)
+            {
+                bool palindrome = false;
+                int count = strr.Length;
+                strr = strr.ToLower();
+                char[] bannedChars = { ' ', '!', '?', '.', '\'' };
+                foreach (char c in bannedChars)
+                {
+                    strr = strr.Replace(c.ToString(), string.Empty);
+                }
+                string reverce = null;
+                for( int i = strr.Length - 1; i >= 0 ; i--)
+                {
+                    reverce += strr[i].ToString();
+                }
+                if( reverce == strr )
+                {
+                    palindrome = true;
+                }
+                return (palindrome , count , reverce);
+            }
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str == "exit") { break; }
+                (bool, int, string) result = Palidrome(str);
+                Console.WriteLine($"Palindrome: {result.Item1}, Lenght: {result.Item2}, Reverced: {result.Item3}");
+            }
+        }
+    }
+}
