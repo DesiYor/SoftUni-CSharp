f142f2d4b3e575ec6b4d2c89ece04908c553a211
Dimitar Zhelyazkov <131526595+DimitarZhelyazkov@users.noreply.github.com>
2023-05-08 02:43:41 +0100

    Update TestCode.cs

--- LinkedIN/TestCode/TestCode.cs
+++ LinkedIN/TestCode/TestCode.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TestCode
 {
-    internal class Program
+    internal class TestCode
     {
         static void Main(string[] args)
         {
+
+            int penPacetsQuantity = int.Parse(Console.ReadLine()); // Вход 1 химикали
+            int markersPacetsQuantity = int.Parse(Console.ReadLine()); // вход 2 маркери
+            int litersPreparationsQuantity = int.Parse(Console.ReadLine()); // вход 3 препарат
+
+            //липсва ти това жход за процент 
+            int percent = int.Parse(Console.ReadLine()); //вход 4 процент 
+
+            double penPrice = penPacetsQuantity * 5.8;
+            double markersPrice = markersPacetsQuantity * 7.20;
+            double litersPrice = litersPreparationsQuantity * 1.20;
+
+
+
+            double totalPrice = penPrice + markersPrice + litersPrice;
+            //double percentReduction = totalPrice * 0.01;
+            double reductionPrice = totalPrice * percent / 100; //
+            //double percentreduction = totalPrice - (totalPrice * 0.01); излишно
+
+            Console.WriteLine(totalPrice - reductionPrice);
         }
     }
 }
